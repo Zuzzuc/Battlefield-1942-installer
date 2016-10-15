7a95eceec2eaeb80c8752060604c96b87b586afc
Love Borgström <loveborgstrom98@gmail.com>
2016-10-15 10:49:57 +0200

    Patch

--- Battlefield 1942 installer.sh
+++ Battlefield 1942 installer.sh
@@ -2,7 +2,11 @@
 # License: The MIT License (MIT)
 # Author Zuzzuc https://github.com/Zuzzuc
 wd="/tmp/bf1942i"
-mkdir $wd
+if [ -d "$wd" ];then
+	echo -e "\n\n\nFound incomplete download(s), continuing"
+else
+	mkdir $wd
+fi
 function Get_E {
 	ExitStatus=$?
 	if [ "$ExitStatus" != "0" ]; then
@@ -28,9 +32,8 @@ else
 	read InstallWine
 	if [ "$InstallWine" == "y" ];then
 		echo -e 'Installing Wine\n'
-		curl -o $wd/Wine.zip -C - http://zuzzuc.com/files/Wine.app.zip
+		curl -o $wd/Wine.zip -C - https://zuzzuc.com/files/Wine.app.zip
 		unzip -o $wd/Wine.zip -d /Applications
-		rm -rf $wd/Wine.zip
 		rm -rf /Applications/__MACOSX
 		echo -e '\nWine has been installed to /Applications/Wine.app\n\n\n'
 	fi
@@ -40,22 +43,21 @@ echo -e 'This is a installer for battlefield 1942.\nThis installer will require 
 sleep 5
 echo -e "\n"
 echo -e "Installing pre built wine files\n"
-curl -o $wd/Winefiles.zip -C - http://www.zuzzuc.com/files/bf1942/WinefilesBF1942.zip
+curl -o $wd/Winefiles.zip -C - https://www.zuzzuc.com/files/bf1942/WinefilesBF1942.zip
 Get_E
 echo -e "\n"
 unzip -o $wd/Winefiles.zip -d ~/
-rm -rf $wd/Winefiles.zip
 rm -rf ~/__MACOSX
 echo -e "\n"
 echo -e "Installing battlefield slim iso\n"
-curl -o ~/Documents/Battlefield\ 1942.iso -C - http://www.zuzzuc.com/files/bf1942/bf1942slim.iso
+curl -o ~/Documents/Battlefield\ 1942.iso -C - https://www.zuzzuc.com/files/bf1942/bf1942slim.iso
 Get_E
 echo -e "\n"
 echo -e "Installing Battlefield launcher\n"
-curl http://zuzzuc.com/files/bf1942/BattlefieldLauncher.sh > /Applications/Battlefield\ Launcher.command
+curl https://zuzzuc.com/files/bf1942/BattlefieldLauncher.sh > /Applications/Battlefield\ Launcher.command
 Get_E
 chmod u+x /Applications/Battlefield\ Launcher.command
-rm -rf $wd
+rm -rf "$wd"
 echo -e "\n\n\n"
 echo 'Done! To launch Battlefield 1942 go to your applications and open "Battlefield launcher.command"'
 echo -e "\n\n\n"
